78414597ae6829506707cf4afe517f2fa83c3b3b
Spinkoo <lespinkoo@gmail.com>
2025-12-12 16:19:50 +0100

    Add utils/ and data/images/ to repository tracking

---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+"""
+Utility functions for 3D reconstruction and visualization
+"""
+from .gif_generator import generate_gif_from_octomap, create_rotating_gif
+
+__all__ = ['generate_gif_from_octomap', 'create_rotating_gif']
+
